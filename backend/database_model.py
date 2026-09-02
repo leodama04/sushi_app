9d1977efb98e2edb326f561110917cbdb84e2d49
Leonardo D'Amario <leonardo.damario@studio.unibo.it>
2026-09-02 19:16:12 +0200

    minor changes in item table

--- backend/database_model.py
+++ backend/database_model.py
@@ -2,4 +2,6 @@ from sqlmodel import Field, SQLModel
 
 class Item(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
-    name: str
+    name: str = Field(min_length=1, max_length=120, index=True)
+    description: str | None = None
+    price: float = Field(gt=0)
